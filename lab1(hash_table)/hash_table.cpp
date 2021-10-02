47e7a3b0bec45571bd9467116846ce351e2244eb
Maxim Kurbatov <m.kurbatov@g.nsu.ru>
2021-10-02 12:43:26 +0700

    added dynamic array - inner class

--- lab1(hash_table)/hash_table.cpp
+++ lab1(hash_table)/hash_table.cpp
@@ -1,18 +1,80 @@
 #include <iostream>
 #include "hash_table.hpp"
-using namespace std;
+#include <cmath>
+#include <cassert>
 
-hash_table::hash_table(int row) {
-    cout << "CONSTRUCTOR" << endl;
+HashTable::HashTable() {
+    IntArray array(MIN_BUF); // Массивчик
+    std::cout << "CONSTRUCTOR" << std::endl;
 }
 
-hash_table::~hash_table() {
-    cout << "DESTUCTOR" << endl;
+HashTable::~HashTable() {
+    std::cout << "DESTUCTOR" << std::endl;
+}
+
+// void HashTable::swap(HashTable& b) {
+//     HashTable* c = this;
+//     this = &b;
+//     b = c;
+// }
+
+
+int calc_main_hash(std::string expression){
+    int len = expression.length();
+    for (int i = 0 ; i < len ; i++) {
+        size_t hash = expression[i] * pow(3.0 , 1.0);
+    }
+    return len;
+}
+
+int HashTable::calc_extra_hash(std::string expression){
+    return 1;
 }
 
 
+IntArray::IntArray(int capacity): array_(new int[capacity]), capacity_(capacity) {
+    std::cout << "Ctor_array" << std::endl;
+}
 
+IntArray::~IntArray() {
+    std::cout << "Dtor_array" << std::endl;
+    delete[] array_;
+}
+
+void IntArray::resize(){
+    int* arr = new int[size_ * 2];
+    capacity_ = capacity_ * 2;
+    for (int i = 0; i < size_; i++){
+        arr[i] = array_[i];
+    }
+    delete [] array_;
+    array_ = arr;
+}
+
+int IntArray::push_back(int i) {
+    if (size_ + 1 == capacity_) resize();
+    array_[size_] = i;
+    size_++;
+    return size_;
+}
+
+int IntArray::at(int i) {
+    assert(i < size_ && i >= 0);
+    return array_[i];
+}
+
+int IntArray::pop_back() {
+    assert(size_ > 0);
+    size_--;
+    return array_[size_];
+}
+
+int IntArray::size() {
+    return size_;
+}
 
 int main(){
+    HashTable table;
+
     return 0;
 }
--- lab1(hash_table)/hash_table.hpp
+++ lab1(hash_table)/hash_table.hpp
@@ -1,8 +1,62 @@
+ #define MIN_BUF 10
+typedef std::string Key;
 
-class hash_table{
-    public:
-        hash_table(int row);
-        ~hash_table();    
-    private:
+typedef struct Value {
+    Value(std::string n, unsigned a = 0) : name(n), age(a) {}
+    std::string name;
+    unsigned age;
+}Value;
 
+class HashTable {
+private:
+    class int_array;
+public:
+    HashTable();
+    ~HashTable();
+
+    HashTable& operator=(const HashTable& b);
+
+    HashTable(const HashTable& b);
+
+    //int calc_main_hash(std::string expression);
+
+    int calc_extra_hash(std::string expression);
+
+    void swap(HashTable& b);
+
+    void clear();
+
+    bool erase(const Key& k);
+
+    bool insert(const Key& k, const Value& v);
+
+    bool contains(const Key& k) const;
+  
+    Value& operator[](const Key& k);
+
+    Value& at(const Key& k);
+
+    const Value& at(const Key& k) const;
+  
+    size_t size() const;
+
+    bool empty() const;
+
+    friend bool operator==(const HashTable& a, const HashTable& b);
+    friend bool operator!=(const HashTable& a, const HashTable& b);
+};
+
+class IntArray {
+public:
+    IntArray(int capacity);
+    ~IntArray();
+private:    
+    int push_back(int i);
+    int at(int i);
+    int pop_back();
+    int size();
+    int *array_;
+    int size_ = 0;
+    int capacity_ = MIN_BUF;
+    void resize();
 };

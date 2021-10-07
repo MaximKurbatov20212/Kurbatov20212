b0731a4b3e539559887212f834f88b7961a4d3dc
Maxim Kurbatov <m.kurbatov@g.nsu.ru>
2021-10-08 01:44:19 +0700

    const Value** , swap , Ctor, print, insert, operator=,clear

--- lab1(hash_table)/hash_table.cpp
+++ lab1(hash_table)/hash_table.cpp
@@ -6,22 +6,25 @@
 void HashTable::print_table() {
     std::cout << "Table" << std::endl;
     for(int i = 0 ; i < capacity_ ; i++) {
-        std::cout << "name: " << array[i].name << " age: " << array[i].age << std::endl;
+        if (array[i]){
+            std::cout << "name: " << array[i]->name << " age: " << array[i]->age << std::endl;
+        }
+        else    {
+            std::cout << " There is noting in " << i << std::endl;    
+        }
     }
     std::cout <<  std::endl;
 }
 
 HashTable::HashTable() {
-    array = new Value[MIN_SIZE];
+    //array = (const Value**)calloc(MIN_SIZE , sizeof(Value*));
+
+    array = new const Value*[MIN_SIZE];
+    std::cout << array;
     for(int i = 0 ; i < MIN_SIZE ; i++) {
-
-        //Value(array[i]); ?
-        //array[i].Value("dsaihd" , 10); ?
-
-        array[i].name = "\0";
-        array[i].age = 0;
+       //std::cout << array[i];
+       array[i] = NULL;
     }
-
     size_ = 0;
     capacity_ = MIN_SIZE;
     std::cout << "Ctor" << std::endl;
@@ -40,7 +43,7 @@ size_t HashTable::capacity() const{
     return capacity_;
 }
 
-int HashTable::calc_main_hash(std::string expression){
+int HashTable::calc_hash(std::string expression){
     int len = expression.length();
     int p = 1;
     unsigned int hash;
@@ -51,20 +54,8 @@ int HashTable::calc_main_hash(std::string expression){
     return hash;
 }
 
-int HashTable::calc_extra_hash(std::string expression){
-    int len = expression.length();
-    int p = 1;
-    unsigned int hash;
-    for (int i = 0 ; i < len ; i++) {
-        hash = expression[i]  % capacity();
-        p *= PRIME_2;
-    }
-    return (hash != 0) ? hash : 1; // step never is null
-    // проблема взаимнопростых чисел
-}
-
 HashTable::HashTable(const HashTable& b){
-    array = new Value[b.capacity_];
+    array = new const Value*[b.capacity_];
     std::copy(b.array , b.array + b.capacity_ , array);
     capacity_ = b.capacity_;
     size_ = b.size_;
@@ -75,132 +66,152 @@ HashTable& HashTable::operator=(const HashTable& b ) {
         delete[] array;
         size_ = b.size_;
         capacity_ = b.capacity_;
-        array = new Value[b.capacity_];
+        array = new const Value*[b.capacity_];
         std::copy(b.array , b.array + b.capacity_ , array);
     }
     return *this;
 }
 
-void HashTable::swap(HashTable& b){
-    Value* c = b.array;
-    b.array = array;
-    array = c;
-    std::swap(size_ , b.size_);
-    std::swap(capacity_ , b.capacity_);
-}
-
-void HashTable::clear() {
-    size_ = 0;
-    for (int i = 0 ; i < capacity_ ; i++) {
-        array[i].name = '\0';
-        array[i].age = 0;
+void copy(const Value** from , const Value** to , size_t capacity_){
+    for (int i = 0; i < capacity_; i++) {
+        if (from[i]) {
+            to[i] = new  Value;
+            to[i] = from[i];
+        }
+        else
+            to[i] = NULL;
     }
 }
 
-bool HashTable::resize() {
-    Value* array_1 = new Value[capacity_];
-    for(int i = 0 ; i < capacity_ ; i++)
-        array_1[i] = array[i];
-    capacity_ *= 2;
-    delete[] array;
-    array = array_1;
+void HashTable::swap(HashTable& b){
+    // this->array <-> b.array
+    const Value** a = array;
+    this->array = b.array;
+    b.array = a;
+
+
+
+    
+    // const Value** a = new const Value * [capacity_];
+    // const Value** c = new const Value* [capacity_];
+
+    // copy(array , a , capacity_);
+    // copy(b.array , c , capacity_);
+
+    // delete[] array; // allocation?
+    // delete[] b.array;
+
+    // array = new const Value*[capacity_];
+    // b.array = new const Value*[capacity_];
+
+    // copy(a , b.array , capacity_);
+    // copy(c , array , capacity_);
+
+    // delete[] a;
+    // delete[] c;
+
+    // std::swap(size_ , b.size_);
+    // std::swap(capacity_ , b.capacity_);
+}
+
+void HashTable::clear() {
+    delete[] this->array;
+}
+
+// bool HashTable::resize() {
+//     Value* array_1 = new Value[capacity_];
+//     for(int i = 0 ; i < capacity_ ; i++)
+//         array_1[i] = array[i];
+//     capacity_ *= 2;
+//     delete[] array;
+//     array = array_1;
 
     //rebuild_table(); добавлю
-}
+//}
+
 bool HashTable::is_occupied(int pos) {
-    if (array[pos].name == "\0") {
+    if(array[pos] == NULL) {
+        array[pos] = new Value();
         return false;
     }
     return true;
 }
 
 bool HashTable::insert(const Key& k, const Value& v) {
-    int hash = calc_main_hash(k);
-    if (!is_occupied(hash)){
-        array[hash] = v;
-        size_++;
-        return true;
+    size_t hash = calc_hash(k);
+    while(is_occupied(hash)) {
+        hash = (hash + 1) % capacity_;
     }
-    hash = calc_extra_hash(k); // Знаю, что нельзя
-    while (true) {
-        if(!is_occupied(hash)) {
-            array[hash] = v;
-            size_++;
-            break;
-        }
-    }
-}
-
-Value& HashTable::operator[](const Key& k) {
-    int step = calc_extra_hash(k);
-    int hash = calc_main_hash(k);
-    for (int i = hash ; ; i += step ) {
-        if (array[i].name == k) {
-            return array[i];        
-        }
-    }
-}
-bool HashTable::erase(const Key& k) {
-        // все или только один key?
-        // at - аналогично
-}
-
-void print_value(Value& a) {
-    std::cout << "name: " << a.name << " age: "<< a.age << std::endl;
-}
-
-bool operator==(const HashTable& a, const HashTable& b) {
-    if (a.size() != b.size()) {
-        return false;
-    }
-
-    else if (a.capacity() != b.capacity()){
-        return false;
-    }
-
-    for(int i = 0 ; i < a.capacity_ ; i++){
-        if (a.array[i].name != b.array[i].name || a.array[i].age != b.array[i].age){
-                return false;
-        }
-    }
+    array[hash] = &v;
     return true;
 }
 
-bool operator!=(const HashTable& a, const HashTable& b) {
-    return !(a == b); // так можно?
-}
+// Value& HashTable::operator[](const Key& k) {
+//     int step = calc_extra_hash(k);
+//     int hash = calc_main_hash(k);
+//     for (int i = hash ; ; i += step ) {
+//         if (array[i].name == k) {
+//             return array[i];        
+//         }
+//     }
+// }
+// bool HashTable::erase(const Key& k) {
+//         // все или только один key?
+//         // at - аналогично
+// }
 
-bool HashTable::empty() const{
-    if (size_ == 0) {
-        return true;
-    }
-    return false;    
-}
+// void print_value(Value& a) {
+//     std::cout << "name: " << a.name << " age: "<< a.age << std::endl;
+// }
+
+// bool operator==(const HashTable& a, const HashTable& b) {
+//     if (a.size() != b.size()) {
+//         return false;
+//     }
+
+//     else if (a.capacity() != b.capacity()){
+//         return false;
+//     }
+
+//     for(int i = 0 ; i < a.capacity_ ; i++){
+//         if (a.array[i].name != b.array[i].name || a.array[i].age != b.array[i].age){
+//                 return false;
+//         }
+//     }
+//     return true;
+// }
+
+// bool operator!=(const HashTable& a, const HashTable& b) {
+//     return !(a == b); // так можно?
+// }
+
+// bool HashTable::empty() const{
+//     if (size_ == 0) {
+//         return true;
+//     }
+//     return false;    
+// }
 
 int main(){
     HashTable table;
-    HashTable b;
-    //table.print_table();
-
     const Value t3 ("John" , 10);
-    const Value t4 ("John" , 20);
-    const Value t1 ("Menson" , 17);
-    const Value t2 ("Alex" , 28);
+    const Value t2 ("Alex" , 23);
+    table.insert("hello" , t3);
+    table.insert("fhbs" , t2);
 
-    table.insert(t3.name , t3);
-    table.insert(t4.name , t4);
+    HashTable a;
+    const Value t0 ("Tom" , 20);
+    const Value t1 ("Bim" , 43);
+    a.insert("hello" , t1);
+    a.insert("fhbs" , t0);
 
-    table.insert(t1.name , t1);
-    table.insert(t2.name , t2);
-    
-    b.insert(t3.name , t3);
-    b.insert(t4.name , t4);
-
-    b.insert(t1.name , t1);
-    b.insert(t2.name , t2);
-    std::cout << (b == table) << std::endl; 
-    
+    a.print_table();
     table.print_table();
-    b.print_table();
+
+    table.swap(a);
+
+    a.print_table();
+    table.print_table();
+
     return 0;
 }
--- lab1(hash_table)/hash_table.hpp
+++ lab1(hash_table)/hash_table.hpp
@@ -1,61 +1,60 @@
-#define MIN_SIZE 13 // простое
-#define PRIME_1 7
-#define PRIME_2 11
-typedef std::string Key;
+    #define MIN_SIZE 13 // простое
+    #define PRIME_1 7
+    #define PRIME_2 11
+    typedef std::string Key;
 
-typedef struct Value {
-    Value() = default;
-    Value(std::string n, unsigned a = 0) : name(n), age(a) {}
-    std::string name;
-    unsigned age;
-}Value;
+    typedef struct Value {
+        Value() = default;
+        Value(std::string n, unsigned a = 0) : name(n), age(a) {}
+        std::string name;
+        unsigned age;
+    }Value;
 
-class HashTable {
-private:
-    Value* array;
-    int size_; 
-    int capacity_;
+    class HashTable {
+    private:
+        const Value** array;
+        int size_; 
+        int capacity_;
 
-    bool is_occupied(int pos);
+        bool is_occupied(int pos);
 
-    bool resize();
+        bool resize();
 
-    int calc_main_hash(std::string expression);
+        int calc_hash(std::string expression);
 
-    int calc_extra_hash(std::string expression);
 
-public:
-    HashTable();
-    ~HashTable();
+    public:
+        HashTable();
+        ~HashTable();
+        
+        HashTable& operator=(const HashTable& b);
+
+        HashTable(const HashTable& b);
+
+        void print_table();
+
+        void swap(HashTable& b);//+
+
+        void clear();
+
+        bool erase(const Key& k);
+
+        bool insert(const Key& k, const Value& v);
+
+        bool contains(const Key& k) const;
     
-    HashTable& operator=(const HashTable& b);
+        Value& operator[](const Key& k);
 
-    HashTable(const HashTable& b);
+        Value& at(const Key& k);
 
-    void print_table();
+        const Value& at(const Key& k) const;
+    
+        size_t size() const ;//+
 
-    void swap(HashTable& b);
+        size_t capacity() const ;//+
 
-    void clear();
+        bool empty() const;
 
-    bool erase(const Key& k);
-
-    bool insert(const Key& k, const Value& v);
-
-    bool contains(const Key& k) const;
-  
-    Value& operator[](const Key& k);
-
-    Value& at(const Key& k);
-
-    const Value& at(const Key& k) const;
-  
-    size_t size() const ;//
-
-    size_t capacity() const ;//
-
-    bool empty() const;//
-
-    friend bool operator==(const HashTable& a, const HashTable& b);//
-    friend bool operator!=(const HashTable& a, const HashTable& b);//
-};
+        friend bool operator==(const HashTable& a, const HashTable& b);//
+        friend bool operator!=(const HashTable& a, const HashTable& b);//
+    };

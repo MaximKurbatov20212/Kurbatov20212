791595e60c1489de7462b44de40c8b57b5a98c58
Maxim <maxim.kurbatov.888@gmail.com>
2021-11-21 22:56:12 +0700

    erase example

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -2,7 +2,25 @@
 #include "hash_table.hpp"
 
 int main() {
-    HashTable h;
-    h.print_table();
-    return 0;
+//rebuild
+    HashTable table;
+    HashTable table1;
+    const Value a("Mark", 10);
+    const Value b("Maxim", 15);
+
+    table.insert("a" , a); 
+    table.insert("e" , b); 
+
+    table.insert("ds" , b); 
+    table.insert("dsf" , a);  
+
+    table.insert("dsaf" , a); 
+    table.insert("we" , b); 
+
+    table.insert("rw" , b); 
+    table.insert("eq" , a); 
+
+    std::cout << table;
+    table.erase("a");
+    std::cout << table;
 }
